cb37cc723708ecfc300f175950908cd16f21c268
yisus <jzavala270591@gmail.com>
2017-04-04 00:05:57 -0500

    Initial Commit

---
+++ MilkLove 3.0/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MilkLove 3.0
+//
+//  Created by YISUS ZAVAlA on 04/04/17.
+//  Copyright © 2017 JESUS ZAVAlA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
